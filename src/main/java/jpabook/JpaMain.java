c17eb6b70b6417bfca90726efef5604b10f65370
hjsong96 <ghkwlsdbwls@gmail.com>
2024-12-26 17:16:17 +0900

    first commit

---
+++ src/main/java/jpabook/JpaMain.java
@@ -0,0 +1,23 @@
+package jpabook;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        tx.commit();
+
+        em.close();
+        emf.close();
+
+    }
+}
